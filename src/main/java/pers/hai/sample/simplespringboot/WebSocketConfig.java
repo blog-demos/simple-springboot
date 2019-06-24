10254e486700dcd5a79be63451ff5b85a8f5eb89
Q-WHai <return_zero0@163.com>
2019-06-24 19:12:58 +0800

    feat:websocket

---
+++ src/main/java/pers/hai/sample/simplespringboot/WebSocketConfig.java
@@ -0,0 +1,23 @@
+package pers.hai.sample.simplespringboot;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * TODO
+ * <p>
+ * Create Time: 2019-06-24 18:18
+ * Last Modify: 2019-06-24
+ *
+ * @author Q-WHai
+ * @see <a href="https://github.com/qwhai">https://github.com/qwhai</a>
+ */
+@Configuration
+public class WebSocketConfig {
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
